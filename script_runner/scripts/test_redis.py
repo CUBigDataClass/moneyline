a28631844edca782ba0bc0dc36dcbb45e8fbe1ef
Andrew <andrewyee@me.com>
2021-03-08 19:13:22 -0700

    add persistent storage for redis database

--- script_runner/scripts/test_redis.py
+++ script_runner/scripts/test_redis.py
@@ -1,9 +1,15 @@
 import redis
 
-db = redis.Redis(host='redis', socket_connect_timeout=1)
+db = redis.Redis(host='redis')
 
 db.ping()
 
 db.set('foo', 'bar')
 
 print(db.get('foo'))
+print(db.get('1'))
+
+for i in range(1000):
+    db.set(str(i), str(i))
+
+db.flushdb()
