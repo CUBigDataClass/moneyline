a4b9a3de88e2b35c792342961b6149e246c18958
Andrew Yee <andrewyee@me.com>
2021-04-21 18:47:16 -0700

    make profit calculator functional

--- moneyline-app/src/app/calculator/calculator.component.ts
+++ moneyline-app/src/app/calculator/calculator.component.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  line = 100;
+  line = -110;
   bet = 0;
+  profit = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calc_profit(){
+    if (this.line > 0){
+      this.profit = "$" + (this.bet * Math.abs(this.line/100)).toFixed(2)
+    }
+    else{
+      this.profit = "$" + (this.bet / Math.abs(this.line/100)).toFixed(2)
+    }
   }
 
 }
